62a0423552b0961f57b4a32ae808e697dab75662
vaibhav Bansode <vaibhavbansode855@gmail.com>
2022-10-10 11:22:07 +0530

    vdeo feature add

--- src/controller/violationController.js
+++ src/controller/violationController.js
@@ -1,37 +1,8 @@
 const violationModel = require("../model/violationModel");
 const vehicleModel = require("../model/vehiclesModel");
 const moment = require("moment");
-const aws = require("aws-sdk");
-// aws.config.update({
-//   accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-//   secretAccessKey: "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",
-//   region: "ap-south-1",
-// });
+const path=require('path')
 
-// let uploadFile = async (file) => {
-//   return new Promise(async function (resolve, reject) {
-//     // Promise.reject(reason) Returns a new Promise object that is rejected with the given reason.
-//     // Promise.resolve(value) Returns a new Promise object that is resolved with the given value.
-//     let s3 = new aws.S3({ apiVersion: "2006-03-01" }); //we will be using s3 service of aws
-
-//     var uploadParams = {
-//       ACL: "public-read",
-//       Bucket: "classroom-training-bucket",
-//       Key: "vaibhav/" + file.originalname,
-//       Body: file.buffer,
-//     };
-
-//     s3.upload(uploadParams, function (err, data) {
-//       if (err) {
-//         return reject({ error: err });
-//       }
-
-//       console.log(data);
-//       console.log(" file uploaded succesfully ");
-//       return resolve(data.Location); // HERE
-//     });
-//   });
-// };
 
 const createViolation = async (req, res) => {
   try {
@@ -72,6 +43,36 @@ const createViolation = async (req, res) => {
         .status(400)
         .send({ status: false, msg: "please enter location" });
     }
+    const media = {
+      Image:[],
+      Video:[]
+  }
+
+let files = req.files // file is the array
+  
+if (files && files.length > 0) {
+
+const acceptableImageExtensions = ['png', 'jpg', 'jpeg', 'jpg']
+const acceptableVideoExtensions = ['mp4','mkv']
+for(let post of req.files){
+if ((acceptableImageExtensions.some(extension => 
+path.extname(post.originalname).toLowerCase() === `.${extension}`)
+)){
+media.Image.push(post.path)
+
+}
+if ((acceptableVideoExtensions.some(extension => 
+path.extname(post.originalname).toLowerCase() === `.${extension}`)
+)){
+media.Video.push(post.path)
+
+}
+}
+}else {
+      return res.status(400).send({ msg: "No file found in request for video" })
+}
+
+ViolationReg.videoUrl = media.Video[0];
 
     let m = moment(
       date,
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,41 @@ const vehicleController=require("../controller/vehicleController")
 const ViolationController=require("../controller/violationController")
 const middleware=require("../middleware/rateLimitter")
 
+const multer = require('multer')
+const path = require('path') // node built-in path package
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, process.cwd() +'/public/')
+    },
+    filename: function (req, file, cb) {
+        const originalName = encodeURIComponent(path.parse(file.originalname).name).replace(/[^a-zA-Z0-9]/g, '')
+        const timestamp = Date.now()
+        const extension = path.extname(file.originalname).toLowerCase()
+        cb(null, originalName + '_' + timestamp + extension)
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 55 * 1024 * 1024 }, // 1 Mb
+    fileFilter: (req, file, callback) => {
+        const acceptableExtensions = ['png','jpg', 'jpeg', 'jpg','mp4']
+        if (!(acceptableExtensions.some(extension => 
+            path.extname(file.originalname).toLowerCase() === `.${extension}`)
+        )) {
+            return callback(new Error(`Extension not allowed, accepted extensions are ${acceptableExtensions.join(',')}`))
+        }
+        //req.file = file
+        callback(null, true)
+    }
+})
+
+const any = ((req, res, next)=>{
+
+    router.use(multer().any())
+    next();
+})
 
 
 // vehicle 
@@ -14,7 +49,7 @@ router.put("/updateRegisterVehicle/:_id",middleware.rateLimiterUsingThirdParty,v
 router.delete("/deleteVhicle/:_id",middleware.rateLimiterUsingThirdParty,vehicleController.deleteVehicleById)
 
 //violation getViolation
-router.post("/registerViolation",middleware.rateLimiterUsingThirdParty,ViolationController.createViolation)
+router.post("/registerViolation",upload.array('file',1),any,middleware.rateLimiterUsingThirdParty,ViolationController.createViolation)
 router.get("/getViolation",middleware.rateLimiterUsingThirdParty,ViolationController.getViolation)
 
 router.get("*", function (req, res) {
